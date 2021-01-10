07d32f2e56b3e00483513f9d13eea6140f39f1a7
かとう えい <n17920517@gmail.com>
2021-01-10 10:59:44 +0900

    ローカル実行時にテーマを最新にできていなかった問題の修正

--- blog_localrun.sh
+++ blog_localrun.sh
@@ -4,7 +4,7 @@ BLOG_SOURCE_DIR=/Users/ei/Documents/GitHub/eikatou.net
 
 # hugo build
 cd ${BLOG_SOURCE_DIR}
-rm -rf themes/blog_theme
+rm -rf themes/eikatou.net_theme
 curl -L https://github.com/eiKatou/eikatou.net_theme/archive/main.zip -o themes/eikatou.net_theme-main.zip
 unzip themes/eikatou.net_theme-main.zip -d themes/
 mv themes/eikatou.net_theme-main themes/eikatou.net_theme
